d42b2bd02049bb935f77bfe1d2a7c9b8dd96b05c
bartMoskala <bartek.moskala@hotmail.com>
2017-05-02 16:11:42 +0200

    add confirmPassword validation and createUser validation

--- OnlineWallet.Infrastructure/Commands/User/CreateUser.cs
+++ OnlineWallet.Infrastructure/Commands/User/CreateUser.cs
@@ -1,11 +1,16 @@
-﻿namespace OnlineWallet.Infrastructure.Commands.User
+﻿using System.ComponentModel.DataAnnotations;
+namespace OnlineWallet.Infrastructure.Commands.User
 {
     public class CreateUser : ICommand
     {
+        [Required]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(32,MinimumLength = 6)]
         public string Password { get; set; }
 
+        [Required]
         public string FullName { get; set; }
 
     }
---
+++ OnlineWallet.UI/wwwroot/lib/js/sign-up-validation.js
@@ -0,0 +1,10 @@
+﻿function check(input) {
+    if (input.value != document.getElementById('password').value) {
+        document.getElementById('confirmPasswordspan').innerHTML = 'Password must be matching.';
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        document.getElementById('confirmPasswordspan').innerHTML = '';
+        input.setCustomValidity('');
+    }
+}
