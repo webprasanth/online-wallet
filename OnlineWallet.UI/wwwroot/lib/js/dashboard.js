e2ff009ba69eadcce8ccf28efe0b164063ab3aac
bartMoskala <bartek.moskala@hotmail.com>
2017-07-11 11:43:32 +0200

    upgrade ajax call

--- OnlineWallet.UI/wwwroot/lib/js/dashboard.js
+++ OnlineWallet.UI/wwwroot/lib/js/dashboard.js
@@ -2,15 +2,15 @@
     $.ajax({
         type: 'GET',
         url: '/api/User/Dashboard',
-        dataType: 'json',
-        success: function (data) {
+        dataType: 'json'
+    })
+    .done(function(data) {
             var transfers = data.transfersCount;
             var deposits = data.depositsCount;
             var withdrawals = data.withdrawalsCount;
             var incomes = data.incomes.toFixed(2);
             var outcomes = data.outcomes.toFixed(2);
 
-            console.log(transfers+" " + deposits + " " + withdrawals);
             if (transfers === 0 && deposits === 0 && withdrawals === 0) {
                 $("#charts").remove();
                 $("body").append('<div class="col-md-12"><h2><center>To display charts you need to do some transaction</center></h2></div>');
@@ -18,12 +18,9 @@
                 createTDWchart(transfers, deposits, withdrawals);
                 createIOchart(incomes, outcomes);
             }
-
-        },
-        error: function (request, status, error) {
+        })
+    .fail(function (request, status, error) {
             alert("Cannot load history");
-        }
-    });
-
+        })
 });
 
