f3c5a205b7a3d21d22f1f896197029938088dc76
bartMoskala <bartek.moskala@hotmail.com>
2017-07-21 22:00:33 +0200

    add knockout to display custom Activity

---
+++ OnlineWallet.UI/wwwroot/js/transactionsvm.js
@@ -0,0 +1,21 @@
+﻿TransactionsViewModel = function (transactions) {
+    self.transactions = ko.observableArray(
+        ko.utils.arrayMap(transactions, function (t) {
+            var user = setUser(t);
+        return {
+            UserFrom: user,
+            UserTo: user,
+            Type: t.Type,
+            Date: t.Date,
+            Amount: (t.Amount).toFixed(2),
+            Id: t.Id
+        };
+        }));
+
+    function setUser(item) {
+        if (item.Type !== "Transfer")
+            return "Internal";
+        else return item.UserFrom;
+    };
+
+};
