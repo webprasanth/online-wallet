4414d3d0c78d5d950f88f205bf29c02bded24e41
bartMoskala <bartek.moskala@hotmail.com>
2017-07-24 12:07:05 +0200

    add Login view component

---
+++ OnlineWallet.UI/ViewComponents/LoginStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineWallet.UI.ViewComponents
+{
+    public class LoginStatus : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userEmailClaim = UserClaimsPrincipal.FindFirst(ClaimTypes.Email);
+                string userEmail = userEmailClaim.Value;
+
+                return View("LoggedIn", userEmail);
+            }
+
+            return View();
+        }
+    }
+}
